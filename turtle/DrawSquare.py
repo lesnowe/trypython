72521381b30dc829cb4f900fc432c20ba036ea59
snowlee <17782414@qq.com>
2021-09-29 22:25:30 +0800

    change color set

--- turtle/DrawSquare.py
+++ turtle/DrawSquare.py
@@ -1,11 +1,10 @@
 # draw a square
 import turtle as t
 t.pensize(10)
-t.pencolor('#9ACD32')
+t.color('#9ACD32', '#FFF68F')
 t.begin_fill()
 for i in range(4):
     t.fd(100)
     t.left(90)
-t.fillcolor('#FFF68F')
 t.end_fill()
 t.done()
