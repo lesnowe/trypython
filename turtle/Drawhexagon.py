27df914bc399e6013013e29dc2e0ba7260fcafab
snowlee <17782414@qq.com>
2021-09-30 19:52:06 +0800

    add draw hexagon

---
+++ turtle/Drawhexagon.py
@@ -0,0 +1,16 @@
+import turtle as t
+t1 = t.Turtle()
+t1.pensize(2)
+t1.color('blue', 'yellow')
+t1.begin_fill()
+for i in range(6):
+    t1.fd(100)
+    t1.left(60)
+t1.end_fill()
+t1.color('blue', 'pink')
+t1.begin_fill()
+for i in range(6):
+    t1.fd(100)
+    t1.right(60)
+t1.end_fill()
+t.done()
